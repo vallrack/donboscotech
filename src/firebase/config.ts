0e98d9b9309f669660e8b6d161e08c7887dd5fb3
Danny Morales <programming2025dan@gmail.com>
2026-03-03 02:33:40 +0000

    conecta a la base de datos:

---
+++ src/firebase/config.ts
@@ -0,0 +1,9 @@
+
+export const firebaseConfig = {
+  apiKey: "AIzaSy...", // This would be populated with real keys in a live environment
+  authDomain: "schedulecontrol-f8b77.firebaseapp.com",
+  projectId: "schedulecontrol-f8b77",
+  storageBucket: "schedulecontrol-f8b77.firebasestorage.app",
+  messagingSenderId: "...",
+  appId: "..."
+};
