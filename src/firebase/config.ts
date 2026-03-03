86e64be4c88e78d625a5b36c7ae3df85ddc87ae2
Your Name <programming2025dan@gmail.com>
2026-03-03 18:13:33 +0000

    creo que con esto ya :
    // Import the functions you need from the SDKs y

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,9 +1,10 @@
 
 export const firebaseConfig = {
-  apiKey: "AIzaSy...", // Los servicios de Firebase Studio inyectarán las claves reales automáticamente
+  apiKey: "AIzaSyAiT1ecETnV6ZexLfxQtXV_mNbnLNs_F5o",
   authDomain: "boscotech-3231f.firebaseapp.com",
   projectId: "boscotech-3231f",
   storageBucket: "boscotech-3231f.firebasestorage.app",
-  messagingSenderId: "...",
-  appId: "..."
+  messagingSenderId: "1084827597694",
+  appId: "1:1084827597694:web:09d4d7dbf185a57298912f",
+  measurementId: "G-24NZ67NK19"
 };
